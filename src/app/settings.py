b01766988ff7fb6716acd84f8239a99ca5a7acb7
Dima Kasperovich <dimakasperovich1720@gmail.com>
2022-02-22 22:14:50 +0200

    lesson code

--- src/app/settings.py
+++ src/app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'user.apps.UserConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.User'
